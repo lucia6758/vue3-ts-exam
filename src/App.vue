9902bdae918be6ad15d232eba4a09a7a030f8ec1
lucia6758 <lucia6758@gmail.com>
2021-02-20 17:51:58 +0900

    필요없는파일제거

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa molestias temporibus, excepturi quibusdam sapiente porro expedita ea voluptate necessitatibus dolor labore itaque nesciunt consequatur assumenda deserunt libero repellendus dignissimos! Ad.</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
   }
 })
 </script>
 
 <style>
+html, body {
+  margin:0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
